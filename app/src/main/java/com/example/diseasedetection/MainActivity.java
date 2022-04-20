3ed4835932d8761a12fef5faee2c957c8df220aa
Mohit Gupta <guptamm.22@gmail.com>
2022-04-20 14:44:06 +0530

    get image from camera

--- app/src/main/java/com/example/diseasedetection/MainActivity.java
+++ app/src/main/java/com/example/diseasedetection/MainActivity.java
@@ -1,13 +1,79 @@
 package com.example.diseasedetection;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView result, demoText, classified, clickHere;
+    ImageView imageView, arrowImage;
+    Button picture;
+
+    int imageSize = 224; // default image size
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result = findViewById(R.id.result);
+        imageView = findViewById(R.id.imageView);
+        picture = findViewById(R.id.button);
+
+        demoText = findViewById(R.id.demoText);
+        clickHere = findViewById(R.id.click_here);
+        arrowImage = findViewById(R.id.demoArrow);
+        classified = findViewById(R.id.classified);
+
+        demoText.setVisibility(View.VISIBLE);
+        clickHere.setVisibility(View.GONE);
+        arrowImage.setVisibility(View.VISIBLE);
+        classified.setVisibility(View.GONE);
+        result.setVisibility(View.GONE);
+
+        picture.setOnClickListener(view -> {
+            // Launch Camera if we have permission
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, 1);
+            }else{
+                // request camera permission id\f don't have
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @androidx.annotation.Nullable Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK){
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            int dimension = Math.min(image.getWidth(), image.getHeight());
+            image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
+            imageView.setImageBitmap(image);
+
+            demoText.setVisibility(View.GONE);
+            clickHere.setVisibility(View.VISIBLE);
+            arrowImage.setVisibility(View.GONE);
+            classified.setVisibility(View.VISIBLE);
+            result.setVisibility(View.VISIBLE);
+
+            image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
